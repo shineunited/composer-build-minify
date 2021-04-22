b27e48ee3e8abdb954b1a9a7c596f3964b790438
Dan Jorgenson <djorgenson@shineunited.com>
2021-04-22 13:38:06 -0500

    Fix MatthiasMullie\Minify\Css capitalization

--- src/Task/MinifyCssTask.php
+++ src/Task/MinifyCssTask.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Composer\IO\IOInterface;
-use MatthiasMullie\Minify\Css as Minifier;
+use MatthiasMullie\Minify\CSS as Minifier;
 
 
 class MinifyCssTask extends Task {
